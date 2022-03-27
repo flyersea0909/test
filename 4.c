23f7994cf57d2294385d7d5b3ff85c6f02184465
flyersea0909 <pzg@cccc58.com>
2022-03-27 18:57:43 +0800

    彭志刚

---
+++ 4.c
@@ -0,0 +1,7 @@
+Filesystem      Size  Used Avail Use% Mounted on
+devtmpfs        1.9G     0  1.9G   0% /dev
+tmpfs           1.9G     0  1.9G   0% /dev/shm
+tmpfs           1.9G  500K  1.9G   1% /run
+tmpfs           1.9G     0  1.9G   0% /sys/fs/cgroup
+/dev/vda1        50G   20G   27G  43% /
+tmpfs           379M     0  379M   0% /run/user/0
